e5487c8de56f9ff26d66175a626d04da80758e07
rashed <sohel011782@yahoo.com>
2020-12-16 15:37:43 -0500

    Added one file and modified one file

---
+++ src/test/java/E2E_002/TC_Add_Custommer_002.java
@@ -0,0 +1,11 @@
+package E2E_002;
+
+public class TC_Add_Custommer_002 {
+	//This is new editing file 
+	//This file will add a new cutommer
+	//this file i am adding new set of coding
+	//this file i am adding new set of coding
+	//this file i am adding new set of coding
+	//this file i am adding new set of coding
+
+}
